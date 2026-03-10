b9ded66a0df5e5bccb8c523f3ca01c671473d978
queapqpq-cloud <queapqpq@gmail.com>
2026-03-10 01:46:53 -0600

    Implement QR code generation for connection updates
    Add QR code generation on connection update

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ index.js
 const qrcode = require("qrcode-terminal")
 package.json
 logs.json
+const qrcode = require("qrcode-terminal")
+
+sock.ev.on("connection.update", ({ qr }) => {
+if (qr) qrcode.generate(qr,{small:true})
+})
